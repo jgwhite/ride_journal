708b100dce32b2d6725b635d559f63b2c6be6c1d
Jamie White <jamie@jgwhite.co.uk>
2012-11-20 09:06:52 +0000

    GPSBabel directive

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# This is a special directive for our Heroku buildpack. Do not delete.
+# require <gpsbabel>
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
